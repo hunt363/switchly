ec6b7d88f63f1ba9c72c8728ca3ee60655207644
hunt363 <kinchitkumar306@gmail.com>
2024-04-16 15:39:20 +0530

    build server

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,12 +5,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const db_1 = __importDefault(require("./config/db"));
+const shortUrl_1 = __importDefault(require("./api/shortUrl"));
 dotenv_1.default.config();
+(0, db_1.default)();
 const port = process.env.PORT || 5001;
+console.log(process.env.PORT);
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
+// app.use(cors({
+//     origin: "https://switchly.vercel.app",
+//     credentials: true,
+// }));
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+// }))
+app.use("/api/", shortUrl_1.default);
 app.listen(port, () => {
     console.log(`server started on port:${port}`);
 });
