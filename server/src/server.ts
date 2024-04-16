302e9a465d19c74e008f3489eef122a591bec8bf
hunt363 <kinchitkumar306@gmail.com>
2024-04-16 17:09:28 +0530

    update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import shortUrl from "./routes/shortUrl";
 dotenv.config();
 connectDB();
 
-const port=process.env.PORT||3000;
+const port=process.env.PORT||5001;
 console.log(process.env.PORT);
 
 const app = express();
