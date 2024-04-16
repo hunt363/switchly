0d9f5a9b083b31a22df567e27a4f6e7eb68810ed
hunt363 <kinchitkumar306@gmail.com>
2024-04-16 16:00:58 +0530

    update server stuff

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db";
-import shortUrl from "./api/shortUrl";
+import shortUrl from "./routes/shortUrl";
 dotenv.config();
 connectDB();
 
@@ -13,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
+
 // app.use(cors({
 //     origin: "https://switchly.vercel.app",
 //     credentials: true,
@@ -28,5 +29,3 @@ app.use("/api/",shortUrl);
 app.listen(port, ()=>{
     console.log(`server started on port:${port}`);
 })
-
-module.exports(app);
