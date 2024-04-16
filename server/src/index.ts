de0d06c280b60220f50a5e552a60d038dd5c16f2
hunt363 <kinchitkumar306@gmail.com>
2024-04-16 15:35:21 +0530

    fix express stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db";
-import shortUrl from "./routes/shortUrl";
+import shortUrl from "./api/shortUrl";
 dotenv.config();
 connectDB();
 
