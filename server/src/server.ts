82ea55cf3a03d1bf174741c7b3c2916f993f9756
hunt363 <kinchitkumar306@gmail.com>
2024-04-16 15:04:16 +0530

    try to fix more stuff

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,15 +11,16 @@ const port=process.env.PORT||5001;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors({
-    origin: "https://switchly.vercel.app",
-    credentials: true,
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: "https://switchly.vercel.app",
+//     credentials: true,
+// }));
 
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-}))
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+// }))
 
 app.use("/api/",shortUrl);
 
